d7baa38bfc9d7b98a7976763bf0ccec02eb785dd
Zetien <jzettien@hotmail.com>
2021-10-21 00:02:49 -0500

    Proyecto agregado a la plataforma

---
+++ fibonacci2.py
@@ -0,0 +1,12 @@
+print ('programa que imprime serie fibonnaci hasta donde digite el usuario') 
+n=int(input('Digite un numero hasta donde va a llegar la serie Fibonacci: \r\n')) 
+a=0
+b=1
+c=0
+contador=1
+while contador<=n:
+    print(c)
+    contador+=1
+    a=b
+    b=c
+    c=a+b
